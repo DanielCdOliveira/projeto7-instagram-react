fbecace8aaa4f6369575e389026b60f19abe3359
Daniel Costa de Oliveira <danielcoliv98@gmail.com>
2022-03-13 15:32:12 -0300

    Navbar creation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import ReactDOM from 'react-dom';
+import Navbar from './Navbar';
+
+
+
+
+
+
+
+function App (){
+return(
+<div className="root">
+    <Navbar />
+    
+    
+    
+    </div>)
+}
+
+let root = document.querySelector(".root")
+ReactDOM.render(<App />, root)
